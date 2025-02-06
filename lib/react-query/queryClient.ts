e5890864fc23b41513c3c24e7fffe975299fdd05
UddeshyaIgnitiv <uddeshya.k@ignitiv.com>
2025-02-06 14:31:20 +0530

    Code reverted to release/production_3.0.1

--- lib/react-query/queryClient.ts
+++ lib/react-query/queryClient.ts
@@ -36,10 +36,6 @@ const queryClientHandler = (error: any, showSnackbar: any) => {
   const status = 'error'
 
   const parsedError = parseGraphQLError(error)
-
-  console.log('This is error ---> ', error)
-  console.log('This is parsed error ---> ', parsedError)
-
   const statusCode = error?.response?.status
     ? error?.response?.status
     : parsedError?.response?.status
@@ -56,36 +52,11 @@ const queryClientHandler = (error: any, showSnackbar: any) => {
     console.warn('API Error:', error) // Log only for other errors
   }
 
-  if (parsedError && parsedError.response.message.includes('Auth declined')) {
-    showSnackbar(
-      'Auth Declined, Error in payment! Please check billing details and please try again!',
-      status
-    )
-  } else if (
-    parsedError &&
-    parsedError.response.message ===
-      'Validation Error: Auth declined: GatewayResponse: 2 This transaction has been declined.'
-  ) {
-    showSnackbar(
-      'Auth Declined, Error in payment! Please check billing details and please try again!',
-      status
-    )
-  } else if (
-    parsedError &&
-    parsedError.response.message.includes('This transaction has been declined')
-  ) {
-    showSnackbar(
-      'Auth Declined, Error in payment! Please check billing details and please try again!',
-      status
-    )
-  }
-
   if (error instanceof SyntaxError && error.message.includes('Auth declined')) {
     showSnackbar(
       'Auth Declined, Error in payment details! Enter correct payment details and please try again!',
       status
     )
-    console.log('Ths is error message in SYntax --> ', error)
   }
   if (error instanceof SyntaxError && error.message.includes('Unexpected token')) {
     //do nothing here for now
