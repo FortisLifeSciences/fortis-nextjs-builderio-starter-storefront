c418c6b34e3afc6646b30fead8f8c8df57ae76d7
Prathmesh Panchwagh <Prathmesh.p@ignitiv.com>
2025-02-13 15:15:42 +0530

    WEB-1234 Wave Tool ADA issue resolved.

--- components/core/Breadcrumbs/KiboBreadcrumbs.tsx
+++ components/core/Breadcrumbs/KiboBreadcrumbs.tsx
@@ -25,16 +25,22 @@ export default function KiboBreadcrumbs({
       <Breadcrumbs separator={separator} {...rest} sx={{ fontSize: '14px', fontWeight: '500' }}>
         {breadcrumbs?.map((item: BreadCrumbType, index) => {
           return (
-            <Link href={item.link as string} key={index} passHref aria-label="breadcrumb-link">
+            <Link
+              href={item.link as string}
+              key={index}
+              passHref
+              aria-label="breadcrumb-link"
+              className={`breadcrumb-linkStyle ${
+                index + 1 < breadcrumbs.length ? 'underline' : ''
+              }`}
+            >
               <Typography
                 variant="body2"
                 color="primary.main"
                 sx={{
                   fontSize: '14px',
                   lineHeight: '20px',
-                  textDecoration: index + 1 < breadcrumbs.length ? 'underline' : 'none',
                   fontWeight: '500',
-                  '&:hover': { textDecoration: 'none' },
                 }}
               >
                 {item.text}
