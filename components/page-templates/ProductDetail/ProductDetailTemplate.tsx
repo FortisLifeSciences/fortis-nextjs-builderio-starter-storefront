38c09d89bf87065dfbdc5e09667d26cf3bbac598
UddeshyaIgnitiv <uddeshya.k@ignitiv.com>
2025-02-27 17:45:02 +0530

    WEB-1323 | Dev complete

--- components/page-templates/ProductDetail/ProductDetailTemplate.tsx
+++ components/page-templates/ProductDetail/ProductDetailTemplate.tsx
@@ -221,6 +221,7 @@ const ProductDetailTemplate = (props: ProductDetailTemplateProps) => {
 
   // console.log('This is updatedProduct ---> ', updatedProduct)
 
+  const sectionTargetUrl = PDPCustomAndBulkDisplayContentSection?.data?.targetUrl
   const [purchaseType, setPurchaseType] = useState<string>(PurchaseTypes.ONETIMEPURCHASE)
   const [selectedFrequency, setSelectedFrequency] = useState<string>('')
   const [isSubscriptionPricingSelected, setIsSubscriptionPricingSelected] = useState<boolean>(false)
@@ -1163,7 +1164,7 @@ const ProductDetailTemplate = (props: ProductDetailTemplateProps) => {
                   content={PDPCustomAndBulkDisplayContentSection}
                   context={{
                     bulkRedirect: () => {
-                      window.location.href = `${siteUrl}bulk-request?Catalog_Num=${variationCodeDynamic}`
+                      window.location.href = `${siteUrl}${sectionTargetUrl}?Catalog_Num=${variationCodeDynamic}`
                     },
                   }}
                 />
