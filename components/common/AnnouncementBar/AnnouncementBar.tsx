9c0a1d13470ff43284d143768e1426adbade2f42
Hanumant Bangar <hanumant.b@ignitiv.com>
2025-06-10 13:18:45 +0530

    WEB-1468: Add section model id to next.config.js file

--- components/common/AnnouncementBar/AnnouncementBar.tsx
+++ components/common/AnnouncementBar/AnnouncementBar.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react'
 
 import { BuilderComponent, builder } from '@builder.io/react'
+import getConfig from 'next/config'
 import { useRouter } from 'next/router'
 
 import type { BuilderContent } from '@builder.io/sdk'
+
+const { publicRuntimeConfig } = getConfig()
+
+const announcementBars = publicRuntimeConfig?.builderIO?.modelKeys?.announcementBar || ''
 
 const AnnouncementBar = () => {
   const [announcements, setAnnouncements] = useState<BuilderContent[] | null>(null)
@@ -12,7 +17,7 @@ const AnnouncementBar = () => {
 
   useEffect(() => {
     async function fetchContent() {
-      const content = await builder.getAll('announcement-bar')
+      const content = await builder.getAll(announcementBars)
       const sorted = content.sort((a, b) => {
         const orderA = a.data?.displayOrder ?? 0
         const orderB = b.data?.displayOrder ?? 0
@@ -21,7 +26,7 @@ const AnnouncementBar = () => {
       setAnnouncements(sorted)
     }
     fetchContent()
-  }, [])
+  }, [router.pathname])
 
   return (
     <>
