4aa5a5fa97fad993f400f4972213cdb07fa49fd3
sanishmdhr96 <mail.sanishmanandhar@gmail.com>
2026-03-31 12:47:20 +0545

    fix: content width fix

--- src/pages/products/[categoryCode].tsx
+++ src/pages/products/[categoryCode].tsx
@@ -4,7 +4,7 @@ import { BuilderComponent, builder } from '@builder.io/react'
 import { Add, ExpandLess, ExpandMore } from '@mui/icons-material'
 import Apps from '@mui/icons-material/Apps'
 import ReorderRounded from '@mui/icons-material/ReorderRounded'
-import { Box, Button, Typography, useMediaQuery } from '@mui/material'
+import { Box, Button, Container, Typography, useMediaQuery } from '@mui/material'
 import algoliasearch from 'algoliasearch'
 import getConfig from 'next/config'
 import Head from 'next/head'
@@ -471,31 +471,36 @@ const CategoryPage: NextPage<CategoryPageType> = (props) => {
         content={props.section}
       />
 
-      <InstantSearch
-        searchClient={searchClient}
-        indexName="products"
-        insights={{
-          onEvent(event) {
-            const { widgetType, eventType, payload, hits } = event
-            const objectIDs = hits?.map((hit) => hit.objectID)
-            if (widgetType === 'ais.hits' && eventType === 'view') {
-              if ((window as any).dataLayer) {
-                window.dataLayer.push({
-                  event: 'Hits Viewed',
-                  algoliaObjectIds: objectIDs,
-                  algoliaIndex: 'products',
-                })
-              }
-            }
-          },
-        }}
+      <Container
+        disableGutters
+        sx={{ maxWidth: '1200px !important', mx: 'auto', px: { xs: 2, md: 0 } }}
       >
-        <MyHitsComponent
-          categoryCode={categoryCode}
-          facets={facets}
-          searchableAttributes={searchableAttributes}
-        />
-      </InstantSearch>
+        <InstantSearch
+          searchClient={searchClient}
+          indexName="products"
+          insights={{
+            onEvent(event) {
+              const { widgetType, eventType, payload, hits } = event
+              const objectIDs = hits?.map((hit) => hit.objectID)
+              if (widgetType === 'ais.hits' && eventType === 'view') {
+                if ((window as any).dataLayer) {
+                  window.dataLayer.push({
+                    event: 'Hits Viewed',
+                    algoliaObjectIds: objectIDs,
+                    algoliaIndex: 'products',
+                  })
+                }
+              }
+            },
+          }}
+        >
+          <MyHitsComponent
+            categoryCode={categoryCode}
+            facets={facets}
+            searchableAttributes={searchableAttributes}
+          />
+        </InstantSearch>
+      </Container>
     </>
   )
 }
