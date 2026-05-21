e5285903441558af4d39bbef85481a34e70fa499
sanishmdhr96 <mail.sanishmanandhar@gmail.com>
2026-05-21 15:44:35 +0545

    WEB-1676 - resolve conflict

--- src/pages/product/[productCode].tsx
+++ src/pages/product/[productCode].tsx
@@ -19,7 +19,7 @@ import { buildProductPath } from '@/lib/helpers'
 import type { CategorySearchParams, MetaData, PageWithMetaData } from '@/lib/types'
 import { generateSchemaMarkups, renderSchemaMarkup } from '@/lib/utils/generate-schema-markup'
 
-import { PrCategory, Product } from '@/lib/gql/types'
+import { FilteredProduct, PrCategory, Product } from '@/lib/gql/types'
 import type {
   GetStaticPathsResult,
   GetStaticPropsContext,
@@ -33,7 +33,7 @@ interface ProductPageType extends PageWithMetaData {
   categoriesTree?: PrCategory[]
   product?: Product
   relatedProducts: any
-  productVariations?: Product[]
+  productVariations?: FilteredProduct[]
   section?: any
   PDPCustomAndBulkDisplayContentSection?: any
   PDPCustomAndBulkDisplaySectionKey?: string
